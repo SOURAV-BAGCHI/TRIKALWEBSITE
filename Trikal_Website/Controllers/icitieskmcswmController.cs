fac68c2c029108c1e8c3784a8390035b6873a54e
Sourav Bagchi <sbagchi19@gmail.com>
2023-06-22 00:22:10 +0530

    Added privacy policy for icties

---
+++ Trikal_Website/Controllers/icitieskmcswmController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Trikal_Website.Controllers
+{
+    public class icitieskmcswmController : Controller
+    {
+        // GET: icitieskmcswm
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+    }
+}
